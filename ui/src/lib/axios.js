df59df76450a0315f36e8a599d3bcd5109eef362
Temitope Bamidele <temibami@gmail.com>
2019-12-15 00:30:24 +0100

    Update axios.js

--- ui/src/lib/axios.js
+++ ui/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const BASE_URL = 'http://127.0.0.1:5000/api/v1';
 // const BASE_URL = process.env.BASE_URL;
-console.log(process.env.BASE_URL)
+
 const instance = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL,
 });
